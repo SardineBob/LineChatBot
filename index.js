e92ae42946b7a703706d62ab71404e9ad4e2e471
SardineBob <yushlai1987@gmail.com>
2017-12-13 16:16:05 +0800

    final

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/*var linebot = require('linebot');
+var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 1551485268,
-  channelSecret: 3a2d8c70b3db9f9655e0e1b49aa89236,
-  channelAccessToken: wmBUSQvR30c7jpYetblFnyKrBse1vzsSGBMvmCpLoGRInDqAUBYXOwyyPheKCI33uHGeOUwziC7fSAfBrCh41YoWmSld+8svtWP8ZEjgHOIW3DWnBZRkFOcvNyzBESjTGP5AroQfp1cvpMa1LgndwAdB04t89/1O/w1cDnyilFU=
+  channelId: "1551485268",
+  channelSecret: "3a2d8c70b3db9f9655e0e1b49aa89236",
+  channelAccessToken: "wmBUSQvR30c7jpYetblFnyKrBse1vzsSGBMvmCpLoGRInDqAUBYXOwyyPheKCI33uHGeOUwziC7fSAfBrCh41YoWmSld+8svtWP8ZEjgHOIW3DWnBZRkFOcvNyzBESjTGP5AroQfp1cvpMa1LgndwAdB04t89/1O/w1cDnyilFU="
 });
 
 bot.on('message', function(event) {
@@ -19,4 +19,4 @@ app.post('/', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
-});*/
+});
